8c86675d697373c445edb38d51e2c97b97271495
CollinBurleigh <collin.s.burleigh@gmail.com>
2024-02-08 02:23:39 -0500

    adjusting color pallet

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -72,7 +72,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/cburleigh1"
+                  href="https://www.instagram.com/itsjustcb1"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
